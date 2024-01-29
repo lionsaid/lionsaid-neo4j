8d1503148b8a2ddba8eb04d9b291dfbe09965f3b
孙伟 <wei.sun@pharmbrain.com>
2024-01-29 19:04:42 +0800

    修改异步方法

--- src/main/java/com/lionsaid/data/Neo4jApplication.java
+++ src/main/java/com/lionsaid/data/Neo4jApplication.java
@@ -1,11 +1,9 @@
 package com.lionsaid.data;
 
-import io.swagger.v3.oas.models.annotations.OpenAPI31;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPI31
 public class Neo4jApplication {
 
     public static void main(String[] args) {
